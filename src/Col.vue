6f00aad8220216670504b73708acea2ad871048f
7777ty <568318359@qq.com>
2020-03-30 18:26:46 +0800

    基本完成了row和col的gutter功能

--- src/Col.vue
+++ src/Col.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
+    <div class="col" :class="colClass" :style="colStyle">
         <slot></slot>
     </div>
 
@@ -7,11 +7,34 @@
 
 <script lang='ts'>
     export default {
+        data(){
+            return{
+                gutter:0,
+
+            }
+
+        },
         props:{
             span:{
                 type:[Number,String],
+            },
+            offset:{
+                type:[Number,String],
+            }
+        },
+        computed:{
+            colClass(){
+                let {span,offset}=this;
+              return   [`span&&col-${span}`, offset&&`offset-${offset}`]
+            },
+            colStyle(){
+                return{
+                    paddingLeft:this.gutter/2+'px',
+                    paddingRight:this.gutter/2+'px',
+                }
             }
         }
+
     }
 </script>
 
@@ -29,5 +52,11 @@
             width: ($n/24)*100%;
         }
     }
+        $class-prefix:offset-;
+        @for $n from 1 through 24{
+            &.#{$class-prefix}#{$n}{
+                margin-left: ($n/24)*100%;
+            }
+        }
     }
 </style>
