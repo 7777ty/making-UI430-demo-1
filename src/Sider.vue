216cb239b16a832f3ff6548b209cd8fb702843fd
7777ty <568318359@qq.com>
2020-03-31 19:30:00 +0800

    网格布局的基本功能完成

---
+++ src/Sider.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="sider">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang='ts'>
+    export default {
+        name:'430sider'
+    }
+
+</script>
+
+<style lang='scss' scoped>
+
+</style>
