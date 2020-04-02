75e9147faf59a5f77f920690801d827f7dcab2c5
7777ty <568318359@qq.com>
2020-04-02 20:04:09 +0800

    优化了三种动画,以及toast的测试用例

--- src/Toast.vue
+++ src/Toast.vue
@@ -1,15 +1,18 @@
 <template>
-    <div class="toast" ref="toast" :class="toastClasses">
-        <div class="message">
-            <slot v-if="!enableHtml"></slot>
-            <div v-else v-html="$slots.default[0]"></div>
+    <div class="wrapper" :class="toastClasses">
+        <div class="toast" ref="toast" >
+            <div class="message">
+                <slot v-if="!enableHtml"></slot>
+                <div v-else v-html="$slots.default[0]"></div>
+            </div>
+            <div class="line" ref="line"></div>
+            <span class="close" v-if="closeButton" @click="onClickClose">{{closeButton.text}}</span>
         </div>
-        <div class="line" ref="line"></div>
-        <span class="close" v-if="closeButton" @click="onClickClose">{{closeButton.text}}</span>
     </div>
+
 </template>
 
-<script lang='ts'>
+<script >
     export default {
         props:{
             autoClose: {
@@ -44,13 +47,12 @@
         mounted(){
             this.updateStyles();
             this.execAutoClose();
-
-
         },
         methods:{
             updateStyles(){
                 this.$nextTick(()=>{
-                    `${this.$refs.line.style.height=this.$refs.toast.getBoundingClientRect().height}px`
+                    this.$refs.line.style.height=
+                    `${this.$refs.toast.getBoundingClientRect().height}px`
                 })
 
             },
@@ -65,9 +67,6 @@
                 this.$el.remove();
                 this.$emit('beforeClose');
                 this.$destroy();
-            },
-            log(){
-              console.log('测试')
             },
             onClickClose()
             {
@@ -94,14 +93,53 @@
     $font-size:14px;
     $toast-min-height:40px;
     $toast-background:rgba(0,0,0,0.75);
+    @keyframes slide-up {
+        0%{opacity: 0;transform: translateY(100%)}
+        100%{opacity: 1;transform: translateY(0%)}
+    }
+    @keyframes slide-down {
+         0%{opacity: 0;transform: translateY(-100%)}
+         100%{opacity: 1;transform: translateY(0%)}
+     }
+    @keyframes fade-in {
+        0%{opacity: 0;}
+        100%{opacity: 1;}
+    }
+    .wrapper{
+        position: fixed;
+        left: 50%;
+        transform: translateX(-50%);
+        &.position-top{
+            top: 0;
+            .toast{
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+                animation: slide-down 500ms;
+            }
+        }
+        &.position-bottom{
+            bottom: 0;
+            .toast{
+                border-bottom-left-radius: 0;
+                border-bottom-right-radius: 0;
+                animation: slide-up 500ms;
+            }
+
+        }
+        &.position-middle{
+            top:50%;
+            transform: translateX(-50%) translateY(-50%);
+            .toast{
+                animation: fade-in 500ms;
+            }
+        }
+    }
     .toast{
         color: white;
-        position: fixed;
         line-height: 1.8;
         min-height: $toast-min-height;
         font-size: $font-size;
         left: 50%;
-
         display: flex;
         align-items: center;
         background: $toast-background;
@@ -120,18 +158,5 @@
             border-left: 1px solid #666;
             margin-left: 16px;
         }
-        &.position-top{
-            top: 0;
-            transform: translateX(-50%);
-        }
-        &.position-bottom{
-            bottom: 0;
-            transform: translateX(-50%);
-        }
-        &.position-middle{
-            top:50%;
-            transform: translate(-50%,-50%);
-        }
     }
-
 </style>
--- src/app.js
+++ src/app.js
@@ -37,17 +37,28 @@ new Vue({
     created(){
 
     },
-    methods:{
-        showToast(){
-            this.$toast('很多文字',{
-                position:'middle',
-                closeButton:{
-                    text:'知道了',
-                    callback(toast){
-                        toast.log();
-                        console.log('用户说他知道了');
+    methods: {
+        showToast1(){
+            this.showToast('top')
+        },
+        showToast2(){
+            this.showToast('middle')
+        },
+        showToast3(){
+            this.showToast('bottom')
+        },
+        showToast(position) {
+            this.$toast(`你的智商目前为 ${parseInt(Math.random() * 100)}。你的智商需要充值！`, {
+                position,
+                enableHtml: false,
+                closeButton: {
+                    text: '已充值',
+                    callback() {
+                        console.log('他说已经充值智商了')
                     }
-                }
+                },
+                autoClose: false,
+                autoCloseDelay: 3
             })
         }
     }
