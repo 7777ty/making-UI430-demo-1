51fdd6a108d5644996926061980cd7e183925b06
7777ty <568318359@qq.com>
2020-04-03 17:12:55 +0800

    完成了tabs组件的切换功能

---
+++ src/Tabs-head.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="tabs-head">
+        <slot></slot>
+        <div class="actions-wrapper">
+            <slot name="actions"></slot>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        inject: ['eventBus'],
+        created () {
+            this.$emit('update:selected', 'tabs-head 抛出的数据')
+        }
+    }
+</script>
+<style scoped lang="scss">
+    $tab-height: 40px;
+    .tabs-head {
+        display: flex;
+        height: $tab-height;
+        justify-content: flex-start;
+        border: 1px solid red;
+        > .actions-wrapper {
+            margin-left: auto;
+        }
+    }
+</style>
--- src/app.js
+++ src/app.js
@@ -12,6 +12,11 @@ import Header from "./Header"
 import Sider from "./Sider"
 import plugin from "./plugin";
 import Toast from "./Toast"
+import Tabs from "./Tabs"
+import TabsHead from "./Tabs-head"
+import TabsItem from "./Tabs-item"
+import TabsBody from "./Tabs-body"
+import TabsPane from "./Tabs-pane"
 
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
@@ -25,41 +30,24 @@ Vue.component('g-footer',Footer);
 Vue.component('g-header',Header);
 Vue.component('g-sider',Sider);
 Vue.component('g-toast',Toast);
+Vue.component('g-tabs-head',TabsHead);
+Vue.component('g-tabs-item',TabsItem);
+Vue.component('g-tabs-body',TabsBody);
+Vue.component('g-tabs-pane',TabsPane);
+Vue.component('g-tabs',Tabs);
+
 Vue.use(plugin);
 
 new Vue({
     el: '#app',
     data: {
-        loading1: false,
-        loading2: true,
-        loading3: false,
+
+            selectedTab: 'sports'
     },
     created(){
 
     },
     methods: {
-        showToast1(){
-            this.showToast('top')
-        },
-        showToast2(){
-            this.showToast('middle')
-        },
-        showToast3(){
-            this.showToast('bottom')
-        },
-        showToast(position) {
-            this.$toast(`你的智商目前为 ${parseInt(Math.random() * 100)}。你的智商需要充值！`, {
-                position,
-                enableHtml: false,
-                closeButton: {
-                    text: '已充值',
-                    callback() {
-                        console.log('他说已经充值智商了')
-                    }
-                },
-                autoClose: false,
-                autoCloseDelay: 3
-            })
-        }
+
     }
 });
